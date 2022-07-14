c9fa77759b97433c7d102023ee2fa81ff989cf6c
Anastasiia Tamonova <ice_asa@mail.ru>
2022-07-14 15:15:53 +0300

    ADD SpotBugs

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -1,8 +1,6 @@
 package ru.netology.statistic;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
